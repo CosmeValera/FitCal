e456eb4dc860744532967df0b6cddc9c368c28f4
CosmeValera <cosmevalerareales@gmail.com>
2023-04-25 16:31:02 +0100

    sidenav created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MiComponenteComponent } from './shared/components/mi-componente/mi-componente.component';
+import { SidenavComponent } from '@shared/components/sidenav/sidenav.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    MiComponenteComponent
+    MiComponenteComponent,
+    SidenavComponent
   ],
   imports: [
     BrowserModule,
