b346088d1582a23bc46e78330a89814fe526e181
CosmeValera <cosmevalerareales@gmail.com>
2023-04-29 22:13:21 +0100

    fix body import in app.module.ts{

--- site/src/app/app.module.ts
+++ site/src/app/app.module.ts
@@ -5,7 +5,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SidenavComponent } from '@shared/components/sidenav/sidenav.component';
-import { BodyComponent } from './body/body.component';
+import { BodyComponent } from './core/body/body.component';
 import { ProfileComponent } from './pages/profile/profile.component';
 import { NutritionComponent } from './pages/nutrition/nutrition.component';
 import { DiaryComponent } from './pages/diary/diary.component';
