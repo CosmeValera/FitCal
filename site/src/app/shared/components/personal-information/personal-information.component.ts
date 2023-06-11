5391fba3d8fd39864f9e1b6ef3b2339f86bb7ec6
angeelsanchez <angel.da@hotmail.es>
2023-06-11 19:03:41 +0200

    altura y peso no permiten valores negativos

--- site/src/app/shared/components/personal-information/personal-information.component.ts
+++ site/src/app/shared/components/personal-information/personal-information.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { DisableRecalculator } from '@shared/services/disableRecalculator.service';
 
 @Component({
@@ -13,22 +13,37 @@ export class PersonalInformationComponent {
 
   nuevoDato: number = 0;
   editarDato = false;
+  previousValue: string = '';
 
-  constructor(private disableRecalculator: DisableRecalculator) {
+  @Output() datoPrincipalChange = new EventEmitter<string>();
 
+  constructor(private disableRecalculator: DisableRecalculator) {}
+
+  onInputChange(event: any) {
+    const value = event.target.value;
+    if ((this.dato === 'Altura:' || this.dato === 'Peso:') && parseInt(value) < 0) {
+      console.error(this.dato === 'Altura:' ? 'La altura no puede ser negativa' : 'El peso no puede ser negativo');
+      this.nuevoDato = parseInt(this.datoPrincipal); // Restablecer el valor anterior
+    } else {
+      this.nuevoDato = parseInt(value);
+    }
   }
 
   guardarDato() {
-    console.log("Guardar datgo");
+    if ((this.dato === 'Altura:' || this.dato === 'Peso:') && this.nuevoDato < 0) {
+      console.error(this.dato === 'Altura:' ? 'La altura no puede ser negativa' : 'El peso no puede ser negativo');
+      this.nuevoDato = parseInt(this.datoPrincipal); // Restablecer el valor anterior
+      return;
+    }
+
     this.datoPrincipal = '' + this.nuevoDato;
     this.disableRecalculator.disableRecalculate();
     this.editarDato = false;
-
+    this.datoPrincipalChange.emit(this.datoPrincipal);
   }
 
   cancelarDato() {
-    console.log("caneclar datgo");
-
     this.editarDato = false;
+    this.nuevoDato = parseInt(this.datoPrincipal); // Restablecer el valor anterior
   }
 }
